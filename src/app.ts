a1d24c94034765479a0192e26541f8d4e7f70687
Dischanell <15079845+Dischanell@users.noreply.github.com>
2024-10-04 11:06:53 +0200

    fix: moved app.ts to src directory

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { json } from 'express'
+import { PORT } from './config.js'
+import { userRouter } from './routes/user.ts'
+
+const app = express()
+app.use(json())
+app.disable('x-powered-by')
+
+app.use('/users', userRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
